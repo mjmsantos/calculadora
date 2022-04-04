6f2a1464449cf11f7619cff046638f5ed4e909cb
mjmsantos <marcos_jsantos@msn.com>
2022-04-04 19:50:28 -0300

    Inclusão da validação que faz /* O replace('.',',') substitui a virgula pelo ponto para permitir fazer o calculo de numero digitados com casas decimas com virgual e depois  "O replace('.',',') substitui o ponto pela virgula para exibir no formato Brasileiro.

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 function somar() {
     var campo1 = document.getElementById('valor1')
     var campo2 = document.getElementById('valor2')
-    var valor1 = parseInt(campo1.value)
-    var valor2 = parseInt(campo2.value)
+    var valor1 = parseFloat(campo1.value.replace(',', '.'))
+    var valor2 = parseFloat(campo2.value.replace(',', '.'))
+
     var resultado = document.getElementById('resultado')
 
     if (campo1.value == "" || campo2.value == "") {
         alert("Os valores são obrigatorios")
         campo1.focus()
     } else {
-        resultado.innerHTML = parseFloat(valor1 + valor2).toFixed(3)
+        /* O replace('.',',') substitui o ponto pela virgual */
+        resultado.innerHTML = parseFloat(valor1 + valor2).toFixed(2).replace('.', ',')
         document.getElementById('multiplica').disabled = true
         document.getElementById('dividi').disabled = true
         document.getElementById('subtrai').disabled = true
@@ -19,15 +21,15 @@ function somar() {
 function dividir() {
     var campo1 = document.getElementById('valor1')
     var campo2 = document.getElementById('valor2')
-    var valor1 = parseInt(campo1.value)
-    var valor2 = parseInt(campo2.value)
+    var valor1 = parseFloat(campo1.value.replace(',', '.'))
+    var valor2 = parseFloat(campo2.value.replace(',', '.'))
     var resultado = document.getElementById('resultado')
 
     if (campo1.value == "" || campo2.value == "") {
         alert("Os valores são obrigatorios")
         campo1.focus()
     } else {
-        resultado.innerHTML = parseFloat(valor1 / valor2).toFixed(2)
+        resultado.innerHTML = parseFloat(valor1 / valor2).toFixed(2).replace('.', ',')
         document.getElementById('soma').disabled = true
         document.getElementById('multiplica').disabled = true
         document.getElementById('subtrai').disabled = true
@@ -37,15 +39,15 @@ function dividir() {
 function multiplicar() {
     var campo1 = document.getElementById('valor1')
     var campo2 = document.getElementById('valor2')
-    var valor1 = parseInt(campo1.value)
-    var valor2 = parseInt(campo2.value)
+    var valor1 = parseFloat(campo1.value.replace(',', '.'))
+    var valor2 = parseFloat(campo2.value.replace(',', '.'))
     var resultado = document.getElementById('resultado')
 
     if (campo1.value == "" || campo2.value == "") {
         alert("Os valores são obrigatorios")
         campo1.focus()
     } else {
-        resultado.innerHTML = parseFloat(valor1 * valor2).toFixed(2)
+        resultado.innerHTML = parseFloat(valor1 * valor2).toFixed(2).replace('.', ',')
         document.getElementById('soma').disabled = true
         document.getElementById('dividi').disabled = true
         document.getElementById('subtrai').disabled = true
@@ -55,15 +57,15 @@ function multiplicar() {
 function subtrair() {
     var campo1 = document.getElementById('valor1')
     var campo2 = document.getElementById('valor2')
-    var valor1 = parseInt(campo1.value)
-    var valor2 = parseInt(campo2.value)
+    var valor1 = parseFloat(campo1.value.replace(',', '.'))
+    var valor2 = parseFloat(campo2.value.replace(',', '.'))
     var resultado = document.getElementById('resultado')
 
     if (campo1.value == "" || campo2.value == "") {
         alert("Os valores são obrigatorios")
         campo1.focus()
     } else {
-        resultado.innerHTML = parseFloat(valor1 - valor2).toFixed(2)
+        resultado.innerHTML = parseFloat(valor1 - valor2).toFixed(2).replace('.', ',')
         document.getElementById('multiplica').disabled = true
         document.getElementById('dividi').disabled = true
         document.getElementById('soma').disabled = true
